c0148b163b4fe7dcabf8713f7454322542305c79
Artur Souto <134516887+ArturSouto@users.noreply.github.com>
2025-11-11 09:51:48 -0300

    remoção da trigger log venda nova

--- SQL/Trigger.sql
+++ SQL/Trigger.sql
@@ -1,27 +1,5 @@
 use concessionaria;
 
-CREATE TABLE IF NOT EXISTS LogVendas (
-                                         idLog INT AUTO_INCREMENT PRIMARY KEY,
-                                         idVenda VARCHAR(10),
-    dataAcao DATETIME,
-    tipoAcao VARCHAR(10),
-    valorAnterior DECIMAL(10,2),
-    valorNovo DECIMAL(10,2)
-    );
-
-
--- vai mostrar quando vender um novo veiculo
-DELIMITER //
-CREATE TRIGGER log_venda_nova
-    AFTER INSERT ON Vende
-    FOR EACH ROW
-BEGIN
-    INSERT INTO LogVendas (idVenda, dataAcao, tipoAcao, valorAnterior, valorNovo)
-    VALUES (NEW.idVenda, NOW(), 'INSERT', NULL, NEW.valorVenda);
-END//
-DELIMITER ;
-
-
 -- nega a inserção de veiculos antigos demais
 DELIMITER //
 CREATE TRIGGER deny_insert_antig_veicu
